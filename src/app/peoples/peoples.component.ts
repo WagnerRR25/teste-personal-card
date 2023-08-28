b9629d314c168d680ddebae2771090dfb525a35c
WagnerRR25 <rodrigueswagner09@gmail.com>
2023-08-28 03:18:42 -0300

    feat: people list, stylization

--- src/app/peoples/peoples.component.ts
+++ src/app/peoples/peoples.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { People } from '../people.model'
+import { PEOPLES } from '../mock-peoples'
 
 @Component({
   selector: 'app-peoples',
   templateUrl: './peoples.component.html',
   styleUrls: ['./peoples.component.scss']
 })
-export class PeoplesComponent implements OnInit{
+export class PeoplesComponent {
   people: People = {
     id: 1,
     name: 'WagnerRR25',
     email: 'rr25wagner@gmail.com'
   };
 
+  peoples = PEOPLES;
+  selectedPeople?: People;
 
-
-  constructor () {}
-
-  ngOnInit(): void {
+  onSelect(people: People): void{
+    this.selectedPeople = people;
   }
 }
